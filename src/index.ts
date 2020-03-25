b354c49e6ab3bfe80d5b17c6a263bcf8d60594b8
dieguezguille <guillermodieguez@outlook.com>
2020-03-25 19:01:00 -0300

    .

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,49 @@
 import * as _ from 'lodash';
 import * as THREE from 'three';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 var renderer = new THREE.WebGLRenderer();
+var loader = new OBJLoader();
+var model: any;
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.BoxGeometry();
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-var cube = new THREE.Mesh(geometry, material);
+var ambientLight = new THREE.AmbientLight(0xcccccc, 0.4);
+scene.add(ambientLight);
 
-scene.add(cube);
+var pointLight = new THREE.PointLight(0xffffff, 0.8);
+camera.add(pointLight);
+scene.add(camera);
 
-camera.position.z = 5;
+loader.load(
+    // resource URL
+    'models/PiletaSketchup.obj',
+    // called when resource is loaded
+    function (object) {
+        model = object;
+        scene.add(model);
+        camera.position.set(0,100,5); camera.lookAt(model.position);
+    },
+    // called when loading progresses
+    function (xhr) {
+        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+    },
+    // called when loading has errors
+    function (error) {
+        console.log(error);
+    }
+);
 
-function animate() {
-    requestAnimationFrame(animate);
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    renderer.render(scene, camera);
-}
+// function animate() {
+//     if (model != null) {
+//         requestAnimationFrame(animate);
+//         model.rotation.x += 0.01;
+//         model.rotation.y += 0.01;
+//         renderer.render(scene, camera);
+//     }
+// }
 
-animate();
+// animate();
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.ts',
+  devServer: {
+    contentBase: './dist',
+  },
+  devtool: 'inline-source-map',
   module: {
     rules: [
       {
