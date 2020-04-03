f09a3baa79f5da75723c103cb8fcc3a52f0439ff
dieguezguille <guillermodieguez@outlook.com>
2020-04-03 16:41:04 -0300

    Updates

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,19 @@ import { Vector3 } from 'three';
 // SCENE
 let camera: THREE.PerspectiveCamera,
     scene: THREE.Scene,
-    renderer: THREE.WebGLRenderer,
-    modelGroup: THREE.Group;
+    renderer: THREE.WebGLRenderer;
+// modelGroup: THREE.Group;
+
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+let movAmount = 0.01; // less is more precise but slower
+
+let models: Array<THREE.Group> = [];
 
 let cameraX: number = 0,
     cameraY: number = 1.7, // altura de la cámara (altura de una persona aprox)
@@ -52,7 +63,7 @@ else {
 }
 
 if (terrainSlope && terrainTilt && helperControlsCheckbox && upButton && downButton && leftButton && rightButton && rotateLeftButton && rotateRightButton) {
-    
+
     terrainSlope.addEventListener("change", onTerrainSlopeChanged);
     terrainTilt.addEventListener("change", onTerrainTiltChanged);
     helperControlsCheckbox.addEventListener("click", onHelperControlsCheckboxClicked);
@@ -110,13 +121,13 @@ function init() {
 
     // model load
     new MTLLoader()
-        .setPath('models/')
-        .load('PiletaSketchup.mtl', function (materials) {
+        .setPath('models/RecessedPool_002/')
+        .load('KayakPool_002.mtl', function (materials) {
             materials.preload();
             new OBJLoader()
                 .setMaterials(materials)
-                .setPath('models/')
-                .load('PiletaSketchup.obj',
+                .setPath('models/RecessedPool_002/')
+                .load('KayakPool_002.obj',
                     (object) => onModelLoaded(object),
                     (xhr) => console.log('Model is ' + (xhr.loaded / xhr.total * 100) + '% loaded'),
                     (error) => console.log(error));
@@ -136,33 +147,36 @@ function onWindowResize() {
 
 function onModelLoaded(loadedModel: THREE.Group) {
     // create group
-    modelGroup = new THREE.Group();
-    modelGroup.name = 'group';
-    scene.add(modelGroup);
+    models[0] = new THREE.Group();
+    models[0].name = 'group';
+    scene.add(models[0]);
 
     // the inside of pool
     loadedModel.renderOrder = 3;
-    modelGroup.add(loadedModel);
+    models[0].add(loadedModel);
 
     // the invisibility box with a hole
-    let cloakGeometry = new THREE.BoxGeometry(6.5, 3.5, 3);
-    cloakGeometry.faces.splice(4, 2); // make hole on top by removing top two triangles
+    // let cloakGeometry = new THREE.BoxGeometry(6.5, 3.5, 3);
+    // cloakGeometry.faces.splice(4, 2); // make hole on top by removing top two triangles
 
-    let cloakMaterial = new THREE.MeshBasicMaterial({
-        colorWrite: false
-    });
+    // let cloakMaterial = new THREE.MeshBasicMaterial({
+    //     colorWrite: false
+    // });
 
-    let cloakMesh = new THREE.Mesh(cloakGeometry, cloakMaterial);
-    cloakMesh.scale.set(1, 1, 1).multiplyScalar(1.01);
-    cloakMesh.position.y = -0.4;
-    cloakMesh.position.z = 0.05;
-    cloakMesh.position.x = 0.04;
-    cloakMesh.renderOrder = 0;
-    modelGroup.add(cloakMesh);
+    // let cloakMesh = new THREE.Mesh(cloakGeometry, cloakMaterial);
+    // cloakMesh.scale.set(1, 1, 1).multiplyScalar(1.01);
+    // cloakMesh.position.y = -0.4;
+    // cloakMesh.position.z = 0.05;
+    // cloakMesh.position.x = 0.04;
+    // cloakMesh.renderOrder = 0;
+    // models[0].add(cloakMesh);
 
-    // final adjustments
-    modelGroup.position.copy(new Vector3(modelGroup.position.x, modelGroup.position.y - 2.6, modelGroup.position.z - 10)); // acomodo el modelo al nivel del suelo
-    modelGroup.scale.set(2, 2, 2);
+    // // final adjustments
+    // models[0].position.copy(new Vector3(models[0].position.x, models[0].position.y - 2.6, models[0].position.z - 10)); // acomodo el modelo al nivel del suelo
+    // models[0].scale.set(2, 2, 2);
+    models[0].position.copy(new Vector3(models[0].position.x, models[0].position.y + 4, models[0].position.z - 10));
+    models[0].scale.set(0.05, 0.05, 0.05);
+
     render();
 }
 
@@ -189,55 +203,58 @@ function onHelperControlsCheckboxClicked(event: any) {
 }
 
 // move actions
-function moveModelDown() {
-    modelGroup.position.z -= 0.01;
-    render();
-}
+function moveModel(direction: Direction){
+    switch (direction){
+        case Direction.Up: {
+            models[0].position.z -= movAmount;
+            break;
+        }
+        case Direction.Down: {
+            models[0].position.z += movAmount;
+            break;
+        }
+        case Direction.Left: {
+            models[0].position.x -= movAmount;
+            break;
+        }
+        case Direction.Right: {
+            models[0].position.x += movAmount;
+            break;
+        }
+    }
 
-function moveModelUp() {
-    modelGroup.position.z += 0.01;
-    render();
-}
-
-function moveModelLeft() {
-    modelGroup.position.x -= 0.01;
-    render();
-}
-
-function moveModelRight() {
-    modelGroup.position.x += 0.01;
     render();
 }
 
 function rotateModelLeft() {
-    modelGroup.rotation.y -= 0.01;
+    models[0].rotation.y -= 0.01;
     render();
 }
 
 function rotateModelRight() {
-    modelGroup.rotation.y += 0.01;
+    models[0].rotation.y += 0.01;
     render();
 }
 
 // model mouse down events
 function onUpButtonMouseDown(event: any) {
     if (timer) return;
-    timer = setInterval(moveModelDown, 10);
+    timer = setInterval(() => moveModel(Direction.Up), 10);
 }
 
 function onDownButtonMouseDown(event: any) {
     if (timer) return;
-    timer = setInterval(moveModelUp, 10);
+    timer = setInterval(() => moveModel(Direction.Down), 10);
 }
 
 function onLeftButtonMouseDown(event: any) {
     if (timer) return;
-    timer = setInterval(moveModelLeft, 10);
+    timer = setInterval(() => moveModel(Direction.Left), 10);
 }
 
 function onRightButtonMouseDown(event: any) {
     if (timer) return;
-    timer = setInterval(moveModelRight, 10);
+    timer = setInterval(() => moveModel(Direction.Right), 10);
 }
 
 function onRotateLeftButtonMouseDown(event: any) {
